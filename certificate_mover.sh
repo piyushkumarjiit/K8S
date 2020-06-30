e1a202b6b64d67ef9bea88d0ed7033b742c5bfe5
Piyush Kumar <piyushkumar.jiit@gmail.com>
2020-06-30 18:54:41 -0500

    Add files via upload

--- certificate_mover.sh
+++ certificate_mover.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 #Author: Piyush Kumar (piyushkumar.jiit@.com)
-if [[ -z ${USER} ]]
+if [[ -z ${USERNAME} ]]
 then
+	#Set the home directory in target server for scp
+	if [[ "$USERNAME" == "root" ]]
+	then
+		TARGET_DIR="/root"
+	else
+		TARGET_DIR="/home/$USERNAME"
+	fi
 	echo "Moving certificates from $HOME to respective locations."
 	mkdir -p /etc/kubernetes/pki/etcd
-	mv /home/${USER}/ca.crt /etc/kubernetes/pki/
-	mv /home/${USER}/ca.key /etc/kubernetes/pki/
-	mv /home/${USER}/sa.pub /etc/kubernetes/pki/
-	mv /home/${USER}/sa.key /etc/kubernetes/pki/
-	mv /home/${USER}/front-proxy-ca.crt /etc/kubernetes/pki/
-	mv /home/${USER}/front-proxy-ca.key /etc/kubernetes/pki/
-	mv /home/${USER}/etcd-ca.crt /etc/kubernetes/pki/etcd/ca.crt
+	mv $TARGET_DIR/ca.crt /etc/kubernetes/pki/
+	mv $TARGET_DIR/ca.key /etc/kubernetes/pki/
+	mv $TARGET_DIR/sa.pub /etc/kubernetes/pki/
+	mv $TARGET_DIR/sa.key /etc/kubernetes/pki/
+	mv $TARGET_DIR/front-proxy-ca.crt /etc/kubernetes/pki/
+	mv $TARGET_DIR/front-proxy-ca.key /etc/kubernetes/pki/
+	mv $TARGET_DIR/etcd-ca.crt /etc/kubernetes/pki/etcd/ca.crt
 	# Quote this line if you are using external etcd
-	mv /home/${USER}/etcd-ca.key /etc/kubernetes/pki/etcd/ca.key
+	mv $TARGET_DIR/etcd-ca.key /etc/kubernetes/pki/etcd/ca.key
 	echo "Certificates moved."
 else
-	echo "USER not defined. Exiting."
+	echo "USERNAME not defined. Exiting."
 	exit 1
 fi
