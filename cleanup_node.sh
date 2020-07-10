ffd3ce68a9b528eac0fdca601308f9e8215f8d06
Piyush Kumar <piyushkumar.jiit@gmail.com>
2020-07-10 12:08:37 -0500

    Add files via upload

--- cleanup_node.sh
+++ cleanup_node.sh
@@ -1,28 +1,6 @@
 #!/bin/bash
 #Author: Piyush Kumar (piyushkumar.jiit@.com)
-
-#Restore the /etc/hosts file
-if [[ -r hosts.txt ]]
-then
-	cat hosts.txt > /etc/hosts
-	echo "Hosts file overwritten."
-else
-	echo "Backup file does not exists."
-fi
-
-#Enable SELinux
-#setenforce 1
-#sed -i 's/^SELINUX=permissive$/SELINUX=enforcing/' /etc/selinux/config
-echo "SELINUX enabled for restart."
-
-#Enable Swap manually
-#sed -ir 's/.*-swap/#&/' /etc/fstab
-echo "Swap enabled for restart."
-
-#Enable and Start firewalld.
-#systemctl enable firewalld
-#systemctl start firewalld
-echo "firewalld enabled and started."
+echo "Cleanup script started."
 
 #Below is newly added. Need to be tested against original ones.
 CURRENT_NODE="$(hostname -I | cut -d" " -f 1)"
@@ -36,7 +14,6 @@ yum -y remove docker-ce kubelet kubeadm kubectl haproxy keepalived
 #sudo apt-get purge kubeadm kubectl kubelet kubernetes-cni kube*   
 #sudo apt-get autoremove  
 
-
 #Delete Repos
 rm -f "/etc/yum.repos.d/docker-ce.repo"
 rm -f "/etc/yum.repos.d/kubernetes.repo"
@@ -48,13 +25,35 @@ rm -f "/etc/docker/daemon.json"
 #Remove folders
 rm -Rf /etc/cni/net.d /root/.kube ~/.kube /var/lib/etcd
 
+#Enable and Start firewalld.
+#systemctl enable firewalld
+#systemctl start firewalld
+echo "firewalld enabled and started."
+
+#Enable Swap manually
+#sed -ir 's/.*-swap/#&/' /etc/fstab
+echo "Swap enabled for restart."
+
+#Enable SELinux
+#setenforce 1
+#sed -i 's/^SELINUX=permissive$/SELINUX=enforcing/' /etc/selinux/config
+echo "SELINUX enabled for restart."
+
+#Restore the /etc/hosts file
+if [[ -r hosts.txt ]]
+then
+	cat hosts.txt > /etc/hosts
+	echo "Hosts file overwritten."
+else
+	echo "Backup file does not exists."
+fi
+
 #Reset the IP Tables
 iptables -F ; iptables -X ; iptables -t nat -F ; iptables -t nat -X; iptables -t mangle -F ; iptables -t mangle -X
 
-
 sysctl -q --system
 
-echo "Script completed."
+echo "Cleanup script completed."
 
 #Restart the node
 #shutdown -r
