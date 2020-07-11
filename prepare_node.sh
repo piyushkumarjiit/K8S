a7d5085ab58e71d706ed7b73629d14a2e8410f73
Piyush Kumar <piyushkumar.jiit@gmail.com>
2020-07-11 00:16:48 -0500

    Add files via upload

--- prepare_node.sh
+++ prepare_node.sh
@@ -138,7 +138,7 @@ else
 	cat <<-'EOF' > /etc/yum.repos.d/kubernetes.repo
 	[kubernetes]
 	name=Kubernetes
-	baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-\$basearch
+	baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-$basearch
 	enabled=1
 	gpgcheck=1
 	repo_gpgcheck=1
