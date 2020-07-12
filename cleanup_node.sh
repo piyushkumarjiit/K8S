b142cabd5532b9d8bb4eee39ca8a02075f38835a
Piyush Kumar <piyushkumar.jiit@gmail.com>
2020-07-11 21:08:24 -0500

    Add files via upload

--- cleanup_node.sh
+++ cleanup_node.sh
@@ -5,27 +5,41 @@ echo "Cleanup script started."
 
 #Below is newly added. Need to be tested against original ones.
 CURRENT_NODE="$(hostname -I | cut -d" " -f 1)"
-kubectl drain $CURRENT_NODE --delete-local-data --force --ignore-daemonsets
-kubectl delete node $CURRENT_NODE
-kubeadm reset
+KUBECTL_AVAILABLE
+KUBEADM_AVAILABLE
+if [[ $KUBECTL_AVAILABLE == 0 ]]
+then
+	kubectl drain $CURRENT_NODE --delete-local-data --force --ignore-daemonsets
+	kubectl delete node $CURRENT_NODE
+	echo "Kubectl delete node called."
+fi
+if [[ KUBEADM_AVAILABLE == 0 ]]
+then
+	kubeadm reset
+	echo "Kubeadm reset called."
+fi
 
 #Remove packages installed by yum
-yum -y remove docker-ce kubelet kubeadm kubectl haproxy keepalived container-selinux
+yum -y remove kubelet kubeadm kubectl
+yum -y remove docker-ce docker-ce-cli containerd.io
+yum -y haproxy keepalived
+yum -y remove container-selinux
 
 yum -y autoremove
 
 #sudo apt-get purge kubeadm kubectl kubelet kubernetes-cni kube*   
 #sudo apt-get autoremove  
 
-#Delete Repos
-rm -f "/etc/yum.repos.d/docker-ce.repo"
-rm -f "/etc/yum.repos.d/kubernetes.repo"
+#Delete Repos we added
+rm -f /etc/yum.repos.d/docker-ce.repo
+rm -f /etc/yum.repos.d/kubernetes.repo
+rm -f /etc/yum.repos.d/*rhcontainerbot:container-selinux.repo*
 
 #Remove extra files created.
-rm -f "/etc/sysctl.d/k8s.conf"
-rm -f "/etc/docker/daemon.json"
-rm -f "~/prepare*.*"
-rm -f "~/cleanup_node*.*"
+rm -f /etc/sysctl.d/k8s.conf
+rm -f /etc/docker/daemon.json
+rm -f ~/prepare*.*
+rm -f ~/cleanup_node*.*
 
 #Remove folders
 rm -Rf /etc/cni/net.d /root/.kube ~/.kube /var/lib/etcd /etc/kubernetes/pki 
