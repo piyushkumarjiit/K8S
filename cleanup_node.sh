64501af4e8dc7de96abd6d7234d86aafefb4b251
Piyush Kumar <piyushkumar.jiit@gmail.com>
2020-07-13 21:01:33 -0500

    Add files via upload

--- cleanup_node.sh
+++ cleanup_node.sh
@@ -5,29 +5,61 @@ echo "Cleanup script initiated from node: $CALLING_NODE_NAME."
 
 #Current Node IP
 CURRENT_NODE_NAME="$(hostname)"
+# kubectl/kubeadm might be installed but with missing config would returns 1
 KUBECTL_AVAILABLE=$(kubectl version > /dev/null 2>&1; echo $?)
 KUBEADM_AVAILABLE=$(kubeadm version > /dev/null 2>&1; echo $?)
+
 if [[ $KUBECTL_AVAILABLE == 0 || $KUBECTL_AVAILABLE == 1 ]]
 then
 	kubectl drain $CURRENT_NODE --delete-local-data --force --ignore-daemonsets
 	kubectl delete node $CURRENT_NODE
 	echo "Kubectl delete node called."
+	yum -y -q remove kubelet kubectl
+	echo "kubelet kubectl removed."
 fi
-if [[ $KUBEADM_AVAILABLE == 0 ]]
+
+if [[ $KUBEADM_AVAILABLE == 0 || $KUBEADM_AVAILABLE == 1 ]]
 then
 	kubeadm reset
 	echo "Kubeadm reset called."
+	yum -y -q remove kubeadm
+	echo "kubeadm removed."
 fi
 
-echo "Pruning Docker"
-docker system prune -af
+DOCKER_AVAILABLE=$(docker --version > /dev/null 2>&1; echo $?)
+if [[ $DOCKER_AVAILABLE == 0 ]]
+then
+	echo "Pruning Docker"
+	docker system prune -af
+	echo "Removing Docker and CRI-O ."
+	yum -y -q remove docker-ce docker-ce-cli containerd.io
+	echo "Docker removed."
+	yum -y -q cri-o
+	echo "cri-o removed."
+fi
+
+KEEPALIVED_AVAILABLE=$(systemctl status keepalived.service > /dev/null 2>&1; echo $?)
+if [[ $KEEPALIVED_AVAILABLE == 0 ]]
+then
+	echo "Removing keepalived."
+	yum -y -q keepalived
+	echo "keepalived removed."
+fi
+
+HAPROXY_AVAILABLE=$(systemctl status haproxy.service > /dev/null 2>&1; echo $?)
+if [[ $HAPROXY_AVAILABLE == 0 ]]
+then
+	echo "Removing haproxy."
+	yum -y -q haproxy
+	echo "haproxy removed."
+fi
 
 #Remove packages installed by yum
-yum -y -q remove kubelet kubeadm kubectl
-yum -y -q remove docker-ce docker-ce-cli containerd.io
-yum -y -q haproxy keepalived
-yum -y -q remove container-selinux
-yum -y -q cri-o
+#yum -y -q remove kubelet kubeadm kubectl
+
+#yum -y -q haproxy keepalived
+#yum -y -q remove container-selinux
+
 echo "Yum remove step completed."
 yum -y -q autoremove
 
@@ -51,7 +83,6 @@ echo "Files created by setup script deleted."
 #echo "Unmounting /aufs"
 #umount /var/lib/docker/aufs
 #umount /var/lib/docker/containers
-
 
 DELETE_FAILED=$(rm -Rf /usr/lib/systemd/system/kubelet.service.d > /dev/null 2>&1; echo $?)
 echo "Delete Falg: "$DELETE_FAILED
