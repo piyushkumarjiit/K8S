1da2a8d69ed4abfb71c5f0a3d434bffbe6609170
Piyush Kumar <piyushkumar.jiit@gmail.com>
2020-07-28 16:33:40 -0500

    Add files via upload

--- cleanup_node.sh
+++ cleanup_node.sh
@@ -139,6 +139,7 @@ echo "Files created by setup script deleted."
 rm -Rf /etc/cni /var/lib/etcd /etc/kubernetes /usr/lib/systemd/system/kubelet.service.d
 rm -Rf /root/.kube ~/.kube
 rm -Rf /etc/docker /var/lib/docker /var/run/docker.sock ~/.docker /usr/bin/docker-compose
+rm -Rf /opt/cni
 
 echo "Docker and Kubernetes config directories deleted."
 #groupdel docker
