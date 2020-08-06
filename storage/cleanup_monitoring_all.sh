0e119842422b8ce54fc48e1c3a964d2856ea534d
Piyush Kumar <piyushkumar.jiit@gmail.com>
2020-08-06 10:43:46 -0500

    Add files via upload

---
+++ storage/cleanup_monitoring_all.sh
@@ -0,0 +1,144 @@
+#!/bin/bash
+#Author: Piyush Kumar (piyushkumar.jiit@.com)
+# ROOK Deployment script. Need to be executed from a host where we have:
+#1. kubectl access
+#2. sudo level access
+#3. internet access to fetch files
+#4. FQDN that was used by ingress
+
+#sudo ./cleanup_monitoring_all.sh | tee cleanup_monitoring.log
+#sudo ./cleanup_monitoring_all.sh |& tee cleanup_monitoring.log
+
+echo "----------- Cleaning Prometheus + Grafana + Alertmanager  ------------"
+#YAML and Git variables
+KUBE_PROMETHEUS_REPO=https://github.com/coreos/kube-prometheus.git
+#export MONITORING_INGRESS_JSONNET=https://raw.githubusercontent.com/coreos/kube-prometheus/b55c2825f7fa4491c6018bd256ef5d7e0b62404c/examples/ingress.jsonnet
+PROMETHEUS_PVC_JSONNET=https://raw.githubusercontent.com/coreos/kube-prometheus/master/examples/prometheus-pvc.jsonnet
+KP_BUILD_SH=https://raw.githubusercontent.com/coreos/kube-prometheus/master/build.sh
+MONITORING_INGRESS_YAML=https://raw.githubusercontent.com/piyushkumarjiit/K8S/master/ingress/monitoring-dashboard-ingress-http.yaml
+GRAFANA_PVC_YAML=https://raw.githubusercontent.com/piyushkumarjiit/K8S/master/monitoring/grafana_pvc.yaml
+
+if [[ $STORAGE_CLASS == "" ]]
+then
+	echo "STORAGE_CLASS is not set, trying to fetch from cluster."
+	#Alternate option to fetch directly
+	STORAGE_CLASS=$(kubectl get storageclass -n monitoring -o json | grep -w '"name":' | xargs | cat | sed "s/,//" | awk -F " " '{print $2}')
+	if [[ $STORAGE_CLASS != "" ]]
+	then
+		echo "Storageclass value set."
+	else
+		echo "Unable to set storageclass: $STORAGE_CLASS. Setting the default value: csi-cephfs. "
+		# Used in the PVC config for Prometheus. Set the value in Name column from the result of the command: kubectl get sc
+		STORAGE_CLASS="csi-cephfs"
+		#sleep 2.
+		#exit 1
+	fi
+fi
+
+if [[ $STORAGE_SIZE == "" ]]
+then
+	echo "STORAGE_SIZE not set. Setting default value of 2Gi."
+	# Size of Prometheus PVC. Allowed value format "1Gi", "2Gi", "5Gi" etc
+	STORAGE_SIZE="2Gi"
+else
+	echo "STORAGE_SIZE already set. Proceeding."
+fi
+
+#github.com/coreos/kube-prometheus/jsonnet/kube-prometheus@release-0.4
+KUBECTL_AVAILABLE=$(kubectl version > /dev/null 2>&1; echo $?)
+
+cd ~
+if [[ -f monitoring-dashboard-ingress-http.yaml ]]
+then
+	echo "Monitoring ingress yaml present."
+else
+	echo "Fetching monitoring ingress yaml from git hub."
+	# Fetch the Ingress YAML from github repo
+	wget -q $MONITORING_INGRESS_YAML -O monitoring-dashboard-ingress-http.yaml
+	# Update the template with your domain name
+	sed -i "s/example.com/$INGRESS_DOMAIN_NAME/g" monitoring-dashboard-ingress-http.yaml
+fi
+
+if [[ -f grafana_pvc.yaml ]]
+then
+	echo "Grafana PVC yaml present."
+else
+	echo "Fetching Grafana PVC yaml from github."
+	# Fetch Grafana PVC YAML from github repo
+	wget -q $GRAFANA_PVC_YAML -O monitoring-grafana_pvc.yaml
+fi
+
+if [[ $KUBECTL_AVAILABLE == 0  ]]
+then
+	# Delete the ingress config YAML
+	kubectl delete -f monitoring-dashboard-ingress-http.yaml
+	echo "Ingress deleted using YAML."
+	sleep 2
+	# Delete PVC for Grafana
+	echo "Trying to delete Grafana PVC"
+	#kubectl patch pvc -n monitoring grafana-pvc -p '{"metadata":{"finalizers":null}}'
+	kubectl delete -f monitoring-grafana_pvc.yaml &
+	PVC_DELETE_STUCK=$(kubectl get pvc -n monitoring grafana-pvc | grep Terminating > /dev/null 2>&1; echo $? )
+	sleep 10
+	if [[ $PVC_DELETE_STUCK == 0 ]]
+	then
+		kubectl patch pvc -n monitoring grafana-pvc -p '{"metadata":{"finalizers":null}}'
+		echo "Had to use patching to fix PVC delete."
+	else
+		echo "PVC for Grafana deleted using YAML."
+	fi
+	#Go to HOME
+	cd ~
+	if [[ -d my-kube-prometheus ]]
+	then
+		echo "Deleting using existing YAML files present in my-kube-prometheus folder."
+		# Got to my-kube-prometheus directory
+		cd my-kube-prometheus
+		# Delete monitoring configs using YAML
+		kubectl delete -f manifests/
+		sleep 2
+		# Delete namespaces and CRDs using YAML
+		kubectl delete -f manifests/setup
+		echo "Done."
+		sleep 2
+		rm -f monitoring-build.sh monitoring-example.jsonnet
+	else
+		echo "Old YAML files are deleted. Run setup in dry run mode to regenrate. Exiting."
+		sleep 2
+		#exit
+	fi
+	# Fetch count of PVC used by monitoring
+	TOTAL_PVC=$(kubectl get pvc -n monitoring -o json | grep -w '"name":' | wc -l)
+	echo "Total PVC in use: "$TOTAL_PVC
+	# Loop to get all their names
+	count=0
+	while [ $count -lt $TOTAL_PVC ]
+	do
+		PVC_NAME=$(kubectl get pvc -n monitoring -o json | grep -w '"name":' | xargs | cat | awk -F "," '{print $count}')
+		kubectl delete -n monitoring pvc $PVC_NAME
+		echo "Deleted PVC: $PVC_NAME"
+		((count++))
+	done
+	echo "All PVCs deleted."
+	sleep 2
+
+else
+	echo "Kubectl unavailable. Unable to delete config using monitoring-dashboard-ingress-http.yaml"
+fi
+
+# Delete binaries for jb, jsonnet and gojsontoyaml
+sudo rm -f /usr/local/bin/jb
+sudo rm -f /usr/local/bin/jsonnet
+sudo rm -f /usr/local/bin/gojsontoyaml
+
+cd ~
+# Delete directories for Go and my-kube-prometheus
+rm -Rf my-kube-prometheus go
+rm -f monitoring-dashboard-ingress-http.yaml monitoring-grafana_pvc.yaml
+
+echo "Script did not remove Go and Git. If required, please remove manually. Cleanup complete."
+sleep 2
+echo "----------- Cleanup complete  ------------"
+# Default Grafana login admin/admin
+
+
---
+++ storage/setup_monitoring_all.sh
@@ -0,0 +1,220 @@
+#!/bin/bash
+#Author: Piyush Kumar (piyushkumar.jiit@.com)
+# ROOK Deployment script. Need to be executed from a host where we have:
+#1. kubectl access
+#2. sudo level access
+#3. internet access to fetch files
+#4. FQDN that can be used by ingress
+
+#sudo ./setup_monitoring_all.sh | tee setup_monitoring.log
+#sudo ./setup_monitoring_all.sh |& tee setup_monitoring.log
+
+echo "----------- Setting up Prometheus + Grafana + Alertmanager  ------------"
+export KUBE_PROMETHEUS_REPO=https://github.com/coreos/kube-prometheus.git
+#export MONITORING_INGRESS_JSONNET=https://raw.githubusercontent.com/coreos/kube-prometheus/b55c2825f7fa4491c6018bd256ef5d7e0b62404c/examples/ingress.jsonnet
+export PROMETHEUS_PVC_JSONNET=https://raw.githubusercontent.com/coreos/kube-prometheus/master/examples/prometheus-pvc.jsonnet
+export KP_BUILD_SH=https://raw.githubusercontent.com/coreos/kube-prometheus/master/build.sh
+export MONITORING_INGRESS_YAML=https://raw.githubusercontent.com/piyushkumarjiit/K8S/master/ingress/monitoring-dashboard-ingress-http.yaml
+export GRAFANA_PVC_YAML=https://raw.githubusercontent.com/piyushkumarjiit/K8S/master/monitoring/grafana_pvc.yaml
+
+if [[ $STORAGE_SIZE == "" ]]
+then
+	echo "STORAGE_SIZE not set. Setting default value of 2Gi."
+	# Size of Prometheus PVC. Allowed value format "1Gi", "2Gi", "5Gi" etc
+	STORAGE_SIZE="2Gi"
+else
+	echo "STORAGE_SIZE already set. Proceeding."
+fi
+
+if [[ $INGRESS_DOMAIN_NAME == "" ]]
+then
+	echo "INGRESS_DOMAIN_NAME not set. Setting a default value."
+	# Domain name to be used by Ingress. Using this grafana URL would become: grafana.<domain.com>
+	INGRESS_DOMAIN_NAME=bifrost.com
+else
+	echo "INGRESS_DOMAIN_NAME already set. Proceeding."
+fi
+
+if [[ $STORAGE_CLASS == "" ]]
+then
+	echo "STORAGE_CLASS is not set, trying to fetch from cluster."
+	#Alternate option to fetch directly
+	STORAGE_CLASS=$(kubectl get storageclass -n monitoring -o json | grep -w '"name":' | xargs | cat | sed "s/,//" | awk -F " " '{print $2}')
+	if [[ $STORAGE_CLASS != "" ]]
+	then
+		echo "Storageclass value set."
+	else
+		echo "Unable to set storageclass: $STORAGE_CLASS. Setting the default value: csi-cephfs. "
+		# Used in the PVC config for Prometheus. Set the value in Name column from the result of the command: kubectl get sc
+		STORAGE_CLASS="csi-cephfs"
+		#sleep 2.
+		#exit 1
+	fi
+fi
+
+# Run mode. Allowed values Deploy/DryRun
+RUN_MODE=""
+
+if [[ $RUN_MODE == "" ]]
+then
+	echo "RUN_MODE not set. Defaulting to Deploy."
+	RUN_MODE=Deploy
+fi
+
+# Install Git
+GIT_INSTALLED=$(git version >/dev/null 2>&1; echo $?)
+if [[  $GIT_INSTALLED -gt 0 ]]
+then
+	echo "Installing git."
+	yum -y -q install git
+fi
+# Install Go
+GO_INSTALLED=$(go version >/dev/null 2>&1; echo $?)
+if [[  $GO_INSTALLED -gt 0 ]]
+then
+	echo "Installing go."
+	yum -y -q install go
+fi
+yum -y -q install go git
+echo "Go and Git installed."
+cd ~
+# Install Jsonnet
+JSONNET_INSTALLED=$(jsonnet -version >/dev/null 2>&1; echo $?)
+if [[  $JSONNET_INSTALLED -gt 0 ]]
+then
+	echo "Installing jsonnet."
+	go get github.com/google/go-jsonnet/cmd/jsonnet
+	chmod 755 ~/go/bin/jsonnet
+	cp ~/go/bin/jsonnet /usr/local/bin/jsonnet
+	cp ~/go/bin/jsonnet /usr/bin/jsonnet
+	JSONNET_INSTALLED=$(jsonnet -version >/dev/null 2>&1; echo $?)
+	if [[  $JSONNET_INSTALLED == 0 ]]
+	then
+		echo "Jsonnet installed."
+	else
+		echo "Jsonnet installation failed."
+	fi
+fi
+# Install Jsonnet Bundler
+JB_INSTALLED=$(jb -h >/dev/null 2>&1; echo $?)
+if [[  $JB_INSTALLED -gt 0 ]]
+then
+	echo "Installing jb."
+	go get github.com/jsonnet-bundler/jsonnet-bundler/cmd/jb
+	chmod 755 ~/go/bin/jb
+	cp ~/go/bin/jb /usr/local/bin/jb
+	cp ~/go/bin/jb /usr/bin/jb	
+	JB_INSTALLED=$(jb -h >/dev/null 2>&1; echo $?)
+	if [[  $JB_INSTALLED == 0 ]]
+	then
+		echo "Jsonnet builder installed."
+	else
+		echo "Jsonnet builder installation failed."
+	fi
+fi
+
+# Install gojsonttoyaml. Used to generate files later
+JSONTOYAML_INSTALLED=$(echo '{"test":"test \\nmultiple"}' | gojsontoyaml >/dev/null 2>&1; echo $?)
+if [[  $JSONTOYAML_INSTALLED -gt 0 ]]
+then
+	echo "Installing gojsontoyaml."
+	go get github.com/brancz/gojsontoyaml
+	chmod 755 ~/go/bin/gojsontoyaml
+	cp ~/go/bin/gojsontoyaml /usr/local/bin/gojsontoyaml
+	cp ~/go/bin/gojsontoyaml /usr/bin/gojsontoyaml
+	JSONTOYAML_INSTALLED=$(echo '{"test":"test \\nmultiple"}' | gojsontoyaml >/dev/null 2>&1; echo $?)
+	if [[  $JSONTOYAML_INSTALLED == 0 ]]
+	then
+		echo "GoJsonToYaml installed."
+	else
+		echo "GoJsonToYaml installation failed."
+	fi
+fi
+
+# Go HOME
+cd ~
+rm -Rf ~/my-kube-prometheus
+# CD to my-kube-prometheus
+mkdir -p my-kube-prometheus; cd my-kube-prometheus
+jb init  # Creates the initial/empty "jsonnetfile.json"
+# Installs all the kube-prometheus jb dependency
+jb install github.com/coreos/kube-prometheus/jsonnet/kube-prometheus@release-0.5
+echo "Kube-Prometheus cloned from repo."
+# Update jb
+jb update
+echo "JB Update completed.."
+# Fetch build.sh from github repo
+wget -q $KP_BUILD_SH -O monitoring-build.sh
+chmod +x monitoring-build.sh
+echo "monitoring-build downloaded and permission updated."
+# Fetch Prometheus PVC example from github repo
+wget -q $PROMETHEUS_PVC_JSONNET -O monitoring-example.jsonnet
+# Update the Storage class in monitoring-example.jsonnet
+sed -i "s/pvc.mixin.spec.withStorageClassName('ssd'),/pvc.mixin.spec.withStorageClassName('$STORAGE_CLASS'),/" monitoring-example.jsonnet
+sed -i "s/pvc.mixin.spec.resources.withRequests({ storage: '100Gi' }/pvc.mixin.spec.resources.withRequests({ storage: '$STORAGE_SIZE' }/" monitoring-example.jsonnet
+echo "PROMETHEUS_PVC_JSONNET downloaded and PVC config updated."
+# Update Jsonnet to include extra namespaces in the cluster. Probably not needed.
+# Fetch namespace jsonnet
+#wget -q https://raw.githubusercontent.com/coreos/kube-prometheus/b55c2825f7fa4491c6018bd256ef5d7e0b62404c/examples/additional-namespaces.jsonnet
+#wget -q https://raw.githubusercontent.com/coreos/kube-prometheus/b55c2825f7fa4491c6018bd256ef5d7e0b62404c/examples/additional-namespaces-servicemonitor.jsonnet
+# Update Ingress for Prometheus, Grafana and Alertmanager. Easier to do through YAML
+#wget -q $MONITORING_INGRESS_JSONNET
+
+# Execute monitoring-build.sh
+./monitoring-build.sh monitoring-example.jsonnet
+
+# Actual deployment in cluster
+if [[ $RUN_MODE == "Deploy" ]]
+then
+	# Create namespaces and CRDs 
+	kubectl create -f manifests/setup
+	# Documentation asks to wait for monitors to come up but they do not until we deploy the pods
+	kubectl get servicemonitors --all-namespaces
+	sleep 30
+	# Updated the grafana yaml to use PVC. Not ideal way to achieve but jsonnet was too much pain.
+	sed -Ez 's/emptyDir: \{\}/persistentVolumeClaim:\n          claimName: grafana-pvc/' ~/my-kube-prometheus/manifests/grafana-deployment.yaml > ~/my-kube-prometheus/manifests/grafana_deployment_updated.yaml
+	rm -f ~/my-kube-prometheus/manifests/grafana-deployment.yaml
+	
+	# Get PVC template for Grafana from github repo and create the PVC
+	wget -q $GRAFANA_PVC_YAML
+	kubectl apply -f grafana_pvc.yaml
+
+	# Apply rest of the YAMLs
+	kubectl create -f manifests/
+	
+	echo "Setup ingress for monitoring dashboards."
+	# Fetch the Ingress YAML from github repo
+	wget -q $MONITORING_INGRESS_YAML
+	# Update the template with your domain name
+	sed -i "s/example.com/$INGRESS_DOMAIN_NAME/g" monitoring-dashboard-ingress-http.yaml
+	# Apply the ingress YAML
+	kubectl apply -f monitoring-dashboard-ingress-http.yaml
+	echo "Ingress setup."
+	sleep 2
+	rm -f monitoring-dashboard-ingress-http.yaml
+	rm -f monitoring-build.sh monitoring-example.jsonnet
+	cd ~
+	#rm -Rf go	
+	CONTINUE_WAITING=$(kubectl get pods -n monitoring | grep grafana | grep Running > /dev/null 2>&1; echo $?)
+	echo -n "Monitoring pods not ready. Waiting ."
+	while [[ $CONTINUE_WAITING != 0 ]]
+	do
+		sleep 20
+		echo -n "."
+	 	CONTINUE_WAITING=$(kubectl get pods -n monitoring | grep grafana | grep Running > /dev/null 2>&1; echo $?)
+	done
+	echo ""
+
+	echo "You can try logging in to Grafana (grafana.$INGRESS_DOMAIN_NAME), Prometheus (prometheus.$INGRESS_DOMAIN_NAME) and Alertmanager (alertmanager.$INGRESS_DOMAIN_NAME)."
+	# Default Grafana login admin/admin
+else
+	echo "Dry Run complete."
+fi
+
+#Delete binaries copied in /usr/bin
+rm -f /usr/bin/jsonnet
+rm -f /usr/bin/jb
+rm -f /usr/bin/gojsontoyaml
+
+echo "----------- All done ----------- "
+
